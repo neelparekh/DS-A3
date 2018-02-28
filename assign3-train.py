3bbce2aef42daf10bbb414b657b89d43cc672db7
Neel Parekh <neel1234@gmail.com>
2018-02-27 22:47:23 -0500

    convert assign3-train to the OG tutorial

--- assign3-train.py
+++ assign3-train.py
@@ -26,99 +26,103 @@ label_conversion = {'n09193705':0 , 'n09246464':1 , 'n09256479':2 , 'n09332890':
 
 ############################ Set up how our model is trained ############################
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-	since = time.time()
+    since = time.time()
 
-	best_model_wts = copy.deepcopy(model.state_dict())
-	best_acc = 0.0
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
 
-	for epoch in range(num_epochs):
-		print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-		print('-' * 10)
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
 
-		# Each epoch has a training and validation phase
-		for phase in ['train', 'val']:
-			if phase == 'train':
-				scheduler.step()
-				model.train(True)  # Set model to training mode
-			else:
-				model.train(False)  # Set model to evaluate mode
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train(True)  # Set model to training mode
+            else:
+                model.train(False)  # Set model to evaluate mode
 
-			running_loss = 0.0
-			running_corrects = 0
+            running_loss = 0.0
+            running_corrects = 0
 
-			# Iterate over data.
-			for data in dataloaders[phase]:
-				# get the inputs
-				inputs, labels = data
+            # Iterate over data.
+            for data in dataloaders[phase]:
+                # get the inputs
+                inputs, labels = data
 
-				# wrap them in Variable
-				if use_gpu:
-					inputs = Variable(inputs.cuda())
-					labels = Variable(labels.cuda())
-				else:
-					inputs, labels = Variable(inputs), Variable(labels)
+                # wrap them in Variable
+                if use_gpu:
+                    inputs = Variable(inputs.cuda())
+                    labels = Variable(labels.cuda())
+                else:
+                    inputs, labels = Variable(inputs), Variable(labels)
 
-				# zero the parameter gradients
-				optimizer.zero_grad()
+                # zero the parameter gradients
+                optimizer.zero_grad()
 
-				# forward
-				outputs = model(inputs)
-				_, preds = torch.max(outputs.data, 1)
-				loss = criterion(outputs, labels)
+                # forward
+                outputs = model(inputs)
+                _, preds = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
 
-				# backward + optimize only if in training phase
-				if phase == 'train':
-					loss.backward()
-					optimizer.step()
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
 
-				# statistics
-				running_loss += loss.data[0] * inputs.size(0)
-				running_corrects += torch.sum(preds == labels.data)
+                # statistics
+                running_loss += loss.data[0] * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
 
-			epoch_loss = running_loss / dataset_sizes[phase]
-			epoch_acc = running_corrects / dataset_sizes[phase]
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects / dataset_sizes[phase]
 
-			print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-				phase, epoch_loss, epoch_acc))
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
 
-			# deep copy the model
-			if phase == 'val' and epoch_acc > best_acc:
-				best_acc = epoch_acc
-				best_model_wts = copy.deepcopy(model.state_dict())
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
 
-		print()
+        print()
 
-	time_elapsed = time.time() - since
-	print('Training complete in {:.0f}m {:.0f}s'.format(
-		time_elapsed // 60, time_elapsed % 60))
-	print('Best val Acc: {:4f}'.format(best_acc))
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
 
-	# load best model weights
-	model.load_state_dict(best_model_wts)
-	return model
-
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
 
 
 ############################ Create datasets and dataloaders for train/val sets ############################
-rd = os.path.join(os.getcwd(),'tiny-imagenet-5')
-trainset = datasets.ImageFolder(root=os.path.join(rd,'train'), transform = transforms.Compose(
-	   [transforms.RandomSizedCrop(224),
-		transforms.RandomHorizontalFlip(),
-		transforms.ToTensor(),
-		transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) 
-	   ]))
-valset   = datasets.ImageFolder(root=os.path.join(rd,'val'), transform = transforms.Compose(
-	   [transforms.Scale(256),
-		transforms.CenterCrop(224),
-		transforms.ToTensor(),
-		transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) 
-	   ]))
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomSizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Scale(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
 
-image_datasets = {'train': trainset, 'val': valset}
-batch_size = 5
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size)  for x in ['train','val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train','val']}
+data_dir = os.path.join(os.getcwd(),'tiny-imagenet-5')
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
+dataloaders    = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=4) for x in ['train', 'val']}
+dataset_sizes  = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names    = image_datasets['train'].classes
 print('number images: ',dataset_sizes)
+
 use_gpu = torch.cuda.is_available()
 
 
@@ -129,7 +133,6 @@ model_conv = models.resnet34(pretrained=True)
 for param in model_conv.parameters():
 	param.requires_grad = False
 num_ftrs = model_conv.fc.in_features
-num_ftrs_out = model_conv.fc.out_features
 model_conv.fc = nn.Linear(num_ftrs, 5)
 
 if use_gpu:
@@ -139,18 +142,70 @@ if use_gpu:
 criterion = nn.CrossEntropyLoss()
 
 # Observe that only parameters of final layer are being optimized as opposed to before.
-optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.01, momentum=0.9)
 
 # Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=3, gamma=0.1)
 
 
 ########################### train our model ############################
 model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=50)
 torch.save(model_conv,'FFE1')
 
+visualize_model(model_conv)
+
+########################### set up our pre-trained model and training params ############################
+#################################### Use as fixed feature extractor #####################################
+
+
+model_ft = models.resnet34(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 5)
+
+if use_gpu:
+    model_ft = model_ft.cuda()
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+########################### train our model ############################
+model_ft = train_model(model_ft, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=50)
+torch.save(model_ft,'FT1')
+
+visualize_model(model_ft)
+
 
 ############################ Helper classes/methods ############################
+
+def visualize_model(model, num_images=6):
+    images_so_far = 0
+    fig = plt.figure()
+
+    for i, data in enumerate(dataloaders['val']):
+        inputs, labels = data
+        if use_gpu:
+            inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+        else:
+            inputs, labels = Variable(inputs), Variable(labels)
+
+        outputs = model(inputs)
+        _, preds = torch.max(outputs.data, 1)
+
+        for j in range(inputs.size()[0]):
+            images_so_far += 1
+            ax = plt.subplot(num_images//2, 2, images_so_far)
+            ax.axis('off')
+            ax.set_title('predicted: {}'.format(class_names[preds[j]]))
+            imshow(inputs.cpu().data[j])
+
+            if images_so_far == num_images:
+                return
+
 # class ToTensor(object):
 #     """Convert ndarrays in sample to Tensors."""
 
